a198d13925ea8e6babe60584aa6f6838de5005bb
steve <steve@steve-laptop.(none)>
2011-01-10 21:30:20 +0000

    Added Some Basic Exceptions

--- m4vToPS3_OO.py
+++ m4vToPS3_OO.py
@@ -3,13 +3,20 @@ import os, re, subprocess, shlex, MySQLdb, sys
 from datetime import datetime
 from pipes import quote
 
-class dbConnect:
+class errorH:
+    def showError(self, e):
+        print "Error %d: %s" % (e.args[0], e.args[1])
+        sys.exit()       
+
+
+class dbConnect(errorH):
     def __init__(self):
-        self.db = MySQLdb.connect(host="192.168.0.5", user="root", passwd="hatter",db="m4vToPS3")
+        try:
+            self.db = MySQLdb.connect(host="192.168.0.5", user="root", passwd="hatter",db="m4vToPS3")
+        except MySQLdb.Error, e:
+            self.showError(e)
 
-
-class m4vToPS3(dbConnect):
-    
+class m4vToPS3(dbConnect): 
     def __init__(self):
         t = datetime.now()
         self.now = t.strftime("%Y-%m-%d")
@@ -17,18 +24,23 @@ class m4vToPS3(dbConnect):
         
     mp4Convert = []
     def getFilesToConvert(self):
-        self.fileLocation = '/usr/local/zend/apache2/htdocs/m4vToPS3/'
-        #self.fileLocation = '/home/steve/Desktop/Share/'
-        m4vReg = re.compile("m4v")
-        if os.path.isdir(self.fileLocation):
-            self.dirContents = os.listdir(self.fileLocation)
-            if ".DS_Store" in self.dirContents:
-                self.dirContents.remove(".DS_Store")
-            for self.film in self.dirContents:
-                if re.search(m4vReg, self.film):
-                    if self.film.replace('m4v','mp4') not in self.dirContents:
-                        self.addFilm(self.film)
-
+        try:
+            self.fileLocation = '/usr/local/zend/apache2/htdocs/m4vToPS3/'
+            #self.fileLocation = '/home/steve/Desktop/Share/'
+            m4vReg = re.compile("m4v")
+            if os.path.isdir(self.fileLocation):
+                self.dirContents = os.listdir(self.fileLocation)
+                if ".DS_Store" in self.dirContents:
+                    self.dirContents.remove(".DS_Store")
+                for self.film in self.dirContents:
+                    if re.search(m4vReg, self.film):
+                        if self.film.replace('m4v','mp4') not in self.dirContents:
+                            self.addFilm(self.film)
+            else:
+                sys.exit("Error: The source folder does not exist")
+        except (OSError,IOError), e:
+            self.showError(e)
+            
     def addFilm(self, film):
         self.mp4Convert.append(film)
 
@@ -59,7 +71,7 @@ class m4vToPS3(dbConnect):
         print self.mp4Convert
 
     def listConverted(self):
-        sql = "SELECT * FROM tbl_converted"
+        sql = "SELECT * FROM tbl_converted ORDER BY dateConverted"
         self.cursor = self.db.cursor()
         self.cursor.execute(sql)
         result = self.cursor.fetchall()
